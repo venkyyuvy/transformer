5fb508825d31cbda94f139a3b60dc2dba47e7df0
Venkatachalam Natchiappan <venky.yuvy@gmail.com>
2023-09-23 08:07:45 +0530

    dataset n dataloader

--- unet/dataset.py
+++ unet/dataset.py
@@ -1,5 +1,5 @@
+from torch.utils.data import Dataset
 import os
-import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
@@ -35,8 +35,6 @@ def PreprocessData(img, mask, target_shape_img, target_shape_mask, path1, path2)
     Returns a NumPy dataset with images as 3-D arrays of desired size
     Please note the masks in this dataset have only one channel
     """
-    # Pull the relevant dimensions for image and mask
-    m = len(img)                     # number of images
     i_h,i_w,i_c = target_shape_img   # pull height, width, and channels of image
     m_h,m_w,m_c = target_shape_mask  # pull height, width, and channels of mask
     
@@ -64,4 +62,46 @@ def PreprocessData(img, mask, target_shape_img, target_shape_mask, path1, path2)
         single_mask = single_mask - 1 # to ensure classes #s start from 0
         data.append((single_img, single_mask))
     return data
+
+
+base_path = "/Users/venkat/Documents/ERA/transformer/unet/data/"
+path1 = base_path + "originals/"
+path2 = base_path + "masks/"
+img, mask = LoadData(path1, path2)
+
+class PetDataset(Dataset):
+    #Init dataset
+    def __init__(self, base_path):
+        path1 = base_path + "originals/"
+        path2 = base_path + "masks/"
+        self.img, self.mask = LoadData(path1, path2)
+        # X_train, X_valid = train_test_split(data, test_size=0.2, random_state=123)
+
+    def __getitem__(self, index):
+        i_h,i_w,i_c = [128, 128, 3]
+        m_h,m_w,m_c = [128, 128, 1]
+        
+        # Define X and Y as number of images along with shape of one image
+        # X = np.zeros((m,i_h,i_w,i_c), dtype=np.float32)
+        # y = np.zeros((m,m_h,m_w,m_c), dtype=np.int32)
+        
+        # convert image into an array of desired shape (3 channels)
+        file = img[index]
+        path = os.path.join(path1, file)
+        single_img = Image.open(path).convert('RGB')
+        single_img = single_img.resize((i_h,i_w))
+        single_img = np.reshape(single_img,(i_h,i_w,i_c)) 
+        single_img = single_img/256.
+        
+        # convert mask into an array of desired shape (1 channel)
+        single_mask_ind = mask[index]
+        path = os.path.join(path2, single_mask_ind)
+        single_mask = Image.open(path)
+        single_mask = single_mask.resize((m_h, m_w))
+        single_mask = np.reshape(single_mask,(m_h,m_w,m_c)) 
+        single_mask = single_mask - 1 # to ensure classes #s start from 0
+        return (single_img, single_mask)
+
+    def __len__(self):
+        return len(self.img)
 
