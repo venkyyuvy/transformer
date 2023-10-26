26a5c31cdd99a4420c9ef7c2b0dce79fa1315fd6
Venkatachalam Natchiappan <venky.yuvy@gmail.com>
2023-10-26 18:36:40 +0530

    entry script

--- S21/gpt.py
+++ S21/gpt.py
@@ -184,31 +184,3 @@ class GPTLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = GPTLanguageModel()
-m = model.to(config.device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
-
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
-
-for iter in range(config.max_iters):
-
-    # every once in a while evaluate the loss on train and val sets
-    if iter % config.eval_interval == 0 or iter == config.max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-
-    # sample a batch of data
-    xb, yb = get_batch('train')
-
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-
-# generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=config.device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
